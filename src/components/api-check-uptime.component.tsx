7caa87d820e6d717575400ddba6c00f121006694
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2023-12-27 20:34:41 -0400

    add tooltip to status chevrons

--- src/components/api-check-uptime.component.tsx
+++ src/components/api-check-uptime.component.tsx
@@ -4,30 +4,174 @@ export function ApiCheckUptimeComponent(props: { results: ApiCheckResult[] }) {
   return (
     <td className="px-6 py-4">
       <ul className="flex flex-row justify-evenly">
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
-        <li className="bg-green-500 w-2 h-10 rounded"></li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
+        <li className="bg-green-500 w-2 h-10 rounded hover:scale-125 has-tooltip">
+          <span className="tooltip rounded bg-gray-200 text-xs -mt-20 -ml-20 flex flex-col justify-evenly p-2 shadow-lg">
+            <p>timestamp: {new Date().toISOString()}</p>
+            <p>duration: {Math.floor(Math.random() * 1000)}</p>
+            <p>status: 200</p>
+          </span>
+        </li>
       </ul>
     </td>
   );
